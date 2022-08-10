cda33bbffadb21d6036ada2ec2fb1ed714c54797
mozhaocong <18825060695@163.com>
2022-08-10 23:35:19 +0800

    feature: 更新

--- src/views/test/index.tsx
+++ src/views/test/index.tsx
@@ -1,6 +1,6 @@
 import { defineComponent, ref } from 'vue'
 import { Button, DatePicker } from 'ant-design-vue'
-import { axiosGet } from '@ht/html-tool'
+import { axiosPost } from '@ht/html-tool'
 import dayjs from 'dayjs'
 export default defineComponent({
 	name: 'test',
@@ -12,10 +12,11 @@ export default defineComponent({
 			// console.log('data', data.value)
 			// console.log('dayjs', dayjs(data.value).format('YY-MM-DD'))
 			// const res = await axiosGet('http://127.0.0.1:7001/user/findAll')
-			const res = await axiosGet('http://127.0.0.1:7001/user/findParams', {
+			// const res = await axiosGet('http://127.0.0.1:7001/user/findParams', {
+			const res = await axiosPost('http://127.0.0.1:7001/user/create', {
 				id: 1,
 				title: '12616',
-				name: 'xx',
+				name: false,
 				// data: dayjs(data.value).format('YY-MM-DD'),
 				date: '2011-12-11',
 			})
