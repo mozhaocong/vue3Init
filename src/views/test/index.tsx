b283a62e3297c3e2262ce38cc753b84ebbbd21f9
莫肇聪 <mozhaocong@qq.com>
2022-08-13 18:02:54 +0800

    feature: 更新

--- src/views/test/index.tsx
+++ src/views/test/index.tsx
@@ -1,6 +1,6 @@
 import { defineComponent, ref } from 'vue'
 import { Button, DatePicker } from 'ant-design-vue'
-import { axiosPost } from '@ht/html-tool'
+import { axiosGet, axiosPost } from '@ht/html-tool'
 import dayjs from 'dayjs'
 export default defineComponent({
 	name: 'test',
@@ -13,14 +13,15 @@ export default defineComponent({
 			// console.log('data', data.value)
 			// console.log('dayjs', dayjs(data.value).format('YY-MM-DD'))
 			// const res = await axiosGet('http://127.0.0.1:7001/user/findAll')
-			// const res = await axiosGet('http://127.0.0.1:7001/user/findParams', {
-			const res = await axiosPost('http://127.0.0.1:7001/user/create', {
+			const res = await axiosGet('http://127.0.0.1:7001/user/findParams', {
+				// const res = await axiosPost('http://127.0.0.1:7001/user/create', {
 				id: 1,
+				size: 5,
 				title: '12616',
-				name: false,
-				age: 1256,
+				name: '张三',
+				// age: 1256,
 				// data: dayjs(data.value).format('YY-MM-DD'),
-				date: '2011-12-11',
+				// updated_at: '2011-12-11',
 			})
 			console.log('res', res)
 		}
